af3a2c97b43f1d70294d47e2f290af236388dfef
Artem <tema.maksimovich@gmail.com>
2021-07-27 00:26:08 +0300

    third commit

--- reverse.js
+++ reverse.js
@@ -1,9 +1,9 @@
 function getReverse(string){
-      let reverseString = "";
-      if(typeof string === 'number') return 'You must enter the string'
+      let reverseString = '';
+      if(typeof string === 'number') return 'You must enter the string';
       for(let i = string.length-1; i >= 0; i--){
             reverseString += string[i];
       }
       return reverseString;
 }
-console.log(getReverse("123456789"));
+console.log(getReverse('123456789'));
