162c625f747b2fe03fb6ae2798e674cd95026b18
ArtemKa2208 <82519968+ArtemKa2208@users.noreply.github.com>
2021-07-27 13:35:13 +0300

    Delete reverse.js

--- reverse.js
+++
@@ -1,9 +0,0 @@
-function getReverse(string){
-      let reverseString = "";
-      if(typeof string === 'number') return 'You must enter the string'
-      for(let i = string.length-1; i >= 0; i--){
-            reverseString += string[i];
-      }
-      return reverseString;
-}
-console.log(getReverse("123456789"));
