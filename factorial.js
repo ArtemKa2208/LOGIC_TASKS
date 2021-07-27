2e30d8a4e553680fb04e2c84a70b83bb331703da
ArtemKa2208 <82519968+ArtemKa2208@users.noreply.github.com>
2021-07-27 13:35:03 +0300

    Delete factorial.js

--- factorial.js
+++
@@ -1,6 +0,0 @@
-function getFactorial(n){
-      if(n < 0) return 'The number must not be negative';
-      if(n === 0) return 1;
-      return n * getFactorial(n-1);
-}
-console.log(getFactorial(9));
