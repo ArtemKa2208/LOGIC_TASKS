ebb62b3c237fdce09416f2fba2f889f64c14c88c
Artem <tema.maksimovich@gmail.com>
2021-07-27 13:32:21 +0300

    third commit

--- constructor.js
+++ constructor.js
@@ -1,11 +1,10 @@
-const user = new User('John','Smith', 20);
-user.print();
 function User(name, lastname, age){
       this.name = name,
       this.lastname = lastname,
-      this.age = age,
-
-      this.print = function(){
-            console.log(`Name: ${this.name}, last name: ${this.lastname}, age: ${this.age}`);
-      };
+      this.age = age  
 }
+User.prototype.print = function(){
+      console.log(`Name: ${this.name}, last name: ${this.lastname}, age: ${this.age}`);
+};
+const user = new User('John','Smith', 20);
+user.print();
